9bc41251be3cbb66b37b880fa2746d7b969beec0
Denis Costa <deniscostadsc@gmail.com>
2013-09-19 17:36:06 -0300

    Melhorando execução dos testes.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,5 +5,5 @@ cd $(dirname $0) && cd ..
 flake8 . --max-line-length=119
 
 coverage erase
-coverage run test/run_tests.py
-coverage report -m --include=chess/* --omit=test/*
+nosetests --with-coverage
+
