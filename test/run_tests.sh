7bc67011af24bb74929955a6df32b6f05504715b
Denis Costa <deniscostadsc@gmail.com>
2013-10-17 20:23:52 -0300

    Update run_tests.sh
    Só verifica o coverage no package correto.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,4 +4,4 @@ cd $(dirname $0) && cd ..
 
 flake8 . --max-line-length=119
 
-nosetests --with-coverage --cover-erase --cover-min-percentage=80
+nosetests --with-coverage --cover-erase --cover-min-percentage=80 --cover-package chess
