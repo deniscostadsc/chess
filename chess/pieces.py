b6ccae20de699f4812f76e86647d3daa9ea64f9d
Denis Costa <deniscostadsc@gmail.com>
2014-05-24 15:23:10 +0200

    Removing repeated code.

--- chess/pieces.py
+++ chess/pieces.py
@@ -1,8 +1,33 @@
+from abc import ABCMeta
+
 from chess import x, y
 
 
 class InvalidChessColor(Exception):
     pass
+
+
+class DiagonalMove(object):
+    '''Mixin that implements diagonal move.
+    '''
+
+    __metaclass__ = ABCMeta
+
+    def can_move_diagonally(self, source, destination):
+        return abs(x.index(source[0]) - x.index(destination[0])) == abs(y.index(destination[1]) - y.index(source[1]))
+
+
+class VerticalAndHorizontalMove(object):
+    '''Mixin that implements vertical and horizontal move.
+    '''
+
+    __metaclass__ = ABCMeta
+
+    def can_move_vertically_or_horizontally(self, source, destination):
+        horizontal = x.index(source[0]) == x.index(destination[0]) and y.index(source[1]) != y.index(destination[1])
+        vertical = x.index(source[0]) != x.index(destination[0]) and y.index(source[1]) == y.index(destination[1])
+
+        return horizontal or vertical
 
 
 class Piece(object):
@@ -33,17 +58,14 @@ class Piece(object):
             self.__moved = value
 
 
-class Bishop(Piece):
+class Bishop(Piece, DiagonalMove):
     def can_move(self, source, destination):
-        return abs(x.index(source[0]) - x.index(destination[0])) == abs(y.index(destination[1]) - y.index(source[1]))
+        return self.can_move_diagonally(source, destination)
 
 
-class Rook(Piece):
+class Rook(Piece, VerticalAndHorizontalMove):
     def can_move(self, source, destination):
-        horizontal = x.index(source[0]) == x.index(destination[0]) and y.index(source[1]) != y.index(destination[1])
-        vertical = x.index(source[0]) != x.index(destination[0]) and y.index(source[1]) == y.index(destination[1])
-
-        return horizontal or vertical
+        return self.can_move_vertically_or_horizontally(source, destination)
 
 
 class King(Piece):
@@ -54,19 +76,10 @@ class King(Piece):
         return x_distance in [0, 1] and y_distance in [0, 1] and x_distance + y_distance != 0
 
 
-class Queen(Piece):
+class Queen(Piece, DiagonalMove, VerticalAndHorizontalMove):
     def can_move(self, source, destination):
-        # move as a Rook {{
-        horizontal = x.index(source[0]) == x.index(destination[0]) and y.index(source[1]) != y.index(destination[1])
-        vertical = x.index(source[0]) != x.index(destination[0]) and y.index(source[1]) == y.index(destination[1])
-
-        if horizontal or vertical:
-            return True
-        # }}
-
-        # move as a Bishop {{
-        return abs(x.index(source[0]) - x.index(destination[0])) == abs(y.index(destination[1]) - y.index(source[1]))
-        # }}
+        return (self.can_move_diagonally(source, destination) or
+                self.can_move_vertically_or_horizontally(source, destination))
 
 
 class Pawn(Piece):
